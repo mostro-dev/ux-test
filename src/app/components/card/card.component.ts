83036db0ec4c82aa97cea8d7e70cd5557588858a
Freddy Orozco <freddycamilo95@gmail.com>
2024-01-24 00:25:23 -0500

    feature/base-components: Add cards info

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { TypographyComponent } from '../typography/typography.component';
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [],
+  imports: [TypographyComponent],
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss'
 })
 export class CardComponent {
-
+	@Input() city: string = "";
+	@Input() price: string = "";
+	@Input() priceCents: string = "";
 }
