fcb0a5021fce137eae1fd21d82b21fe6cadcb325
Freddy Orozco <freddycamilo95@gmail.com>
2024-01-23 21:11:16 -0500

    feature/base-components: Add menu bar to landing

--- src/app/pages/landing-page/landing-page.component.ts
+++ src/app/pages/landing-page/landing-page.component.ts
@@ -3,12 +3,13 @@ import { ButtonComponent } from "../../components/button/button.component";
 import { FooterComponent } from "../../components/footer/footer.component";
 import { LayoutComponent } from "../../components/layout/layout.component";
 import { TypographyComponent } from "../../components/typography/typography.component";
+import { MenuBarComponent } from "../../components/menu-bar/menu-bar.component";
 
 
 @Component({
 	selector: "app-landing-page",
 	standalone: true,
-	imports: [TypographyComponent, ButtonComponent, FooterComponent, LayoutComponent],
+	imports: [TypographyComponent, ButtonComponent, FooterComponent, LayoutComponent, MenuBarComponent],
 	templateUrl: "./landing-page.component.html",
 	styleUrl: "./landing-page.component.scss",
 })
