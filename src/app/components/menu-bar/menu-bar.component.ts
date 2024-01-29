1fa7045a9cd30025f9d92152e178b488730ed63b
Freddy Orozco <freddycamilo95@gmail.com>
2024-01-29 18:09:36 -0500

    feature/menu-and-footer: Add sub menu items behavior

--- src/app/components/menu-bar/menu-bar.component.ts
+++ src/app/components/menu-bar/menu-bar.component.ts
@@ -1,18 +1,33 @@
 import { Component } from "@angular/core";
 import { LayoutComponent } from "../layout/layout.component";
 import { TypographyComponent } from "../typography/typography.component";
+import { CommonModule } from "@angular/common";
 
 @Component({
 	selector: "app-menu-bar",
 	standalone: true,
-	imports: [LayoutComponent, TypographyComponent],
+	imports: [CommonModule, LayoutComponent, TypographyComponent],
 	templateUrl: "./menu-bar.component.html",
 	styleUrl: "./menu-bar.component.scss",
 })
 export class MenuBarComponent {
 	public isActive: boolean = false;
+	public selectedMenuItem: number | null = null;
 
 	toggleMenu() {
 		this.isActive = !this.isActive;
+	}
+
+	selectMenuItem(index: number) {
+		console.log(index);
+		if (this.selectedMenuItem === index) {
+			this.selectedMenuItem = null;
+		} else {
+			this.selectedMenuItem = index;
+		}
+	}
+
+	getTypographyWeight(item: number) {
+		return this.selectedMenuItem === item ? "bold" : "regular";
 	}
 }
