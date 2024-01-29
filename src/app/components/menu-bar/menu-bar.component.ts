8c3aa9e831df1b244b0b2d6f6b522eae61fbdfd1
Freddy Orozco <freddycamilo95@gmail.com>
2024-01-29 18:19:55 -0500

    feature/menu-and-footer: Update menubar to get data from .ts file

--- src/app/components/menu-bar/menu-bar.component.ts
+++ src/app/components/menu-bar/menu-bar.component.ts
@@ -14,6 +14,32 @@ export class MenuBarComponent {
 	public isActive: boolean = false;
 	public selectedMenuItem: number | null = null;
 
+	public menuItems = [
+		{
+			id: 0,
+			label: "Check-in",
+			subItems: [
+				{ id: 0, label: "My Orders", href: "/" },
+				{ id: 1, label: "Check-in", href: "/" },
+			],
+		},
+		{
+			id: 1,
+			label: "Manage my booking",
+			subItems: [{ id: 0, label: "My booking", href: "/" }],
+		},
+		{
+			id: 2,
+			label: "Information",
+			subItems: [
+				{ id: 0, label: "Help Center", href: "/" },
+				{ id: 1, label: "Flight state", href: "/" },
+				{ id: 2, label: "Airplanes", href: "/" },
+				{ id: 3, label: "Routes", href: "/" },
+			],
+		},
+	];
+
 	toggleMenu() {
 		this.isActive = !this.isActive;
 	}
